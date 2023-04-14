a0b4b123bfb369cc13e89c0456c18663dbe10b35
ipekgulsencelik <ipekgulsen.celik@gmail.com>
2023-04-14 17:39:02 +0300

    adding course class

---
+++ Educator.EntityLayer/Concrete/Course.cs
@@ -0,0 +1,12 @@
+﻿namespace Edukator.EntityLayer.Concrete
+{
+    public class Course
+    {
+        public int CourseID { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string ImageURL { get; set; }
+        public decimal Price { get; set; }
+        public int Review { get; set; }
+    }
+}
