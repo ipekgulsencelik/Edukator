a3d10d154132ea7730ea774ccf33c17532d09fc9
ipekgulsencelik <ipekgulsen.celik@gmail.com>
2023-04-14 17:38:32 +0300

    adding category class

---
+++ Educator.EntityLayer/Concrete/Category.cs
@@ -0,0 +1,8 @@
+﻿namespace Edukator.EntityLayer.Concrete
+{
+    public class Category
+    {
+        public int CategoryID { get; set; }
+        public string CategoryName { get; set; }
+    }
+}
