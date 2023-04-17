0fe127ba333da9225faa4fa9b7ee0886ae7509eb
ipekgulsencelik <ipekgulsen.celik@gmail.com>
2023-04-17 22:55:24 +0300

    adding interface for table

---
+++ Edukator.DataAccessLayer/Abstract/IGenericDAL.cs
@@ -0,0 +1,11 @@
+﻿namespace Edukator.DataAccessLayer.Abstract
+{
+    public interface IGenericDAL<T> where T : class
+    {
+        void Insert(T entity);
+        void Delete(T entity);
+        void Update(T entity);
+        List<T> GetList();
+        T GetByID(int id);
+    }
+}
