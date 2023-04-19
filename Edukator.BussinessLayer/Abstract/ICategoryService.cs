046c0f629b2136e4d3a2dba946c46392d212c46d
ipekgulsencelik <ipekgulsen.celik@gmail.com>
2023-04-19 15:57:11 +0300

    adding service interface

---
+++ Edukator.BussinessLayer/Abstract/ICategoryService.cs
@@ -0,0 +1,8 @@
+﻿using Edukator.EntityLayer.Concrete;
+
+namespace Edukator.BussinessLayer.Abstract
+{
+    public interface ICategoryService : IGenericService<Category>
+    {
+    }
+}
---
+++ Edukator.BussinessLayer/Abstract/IServiceService.cs
@@ -0,0 +1,8 @@
+﻿using Edukator.EntityLayer.Concrete;
+
+namespace Edukator.BussinessLayer.Abstract
+{
+    public interface IServiceService : IGenericService<Service>
+    {
+    }
+}
