affefff3799b84ab4f4c3c732fbb288ecfc025dc
ipekgulsencelik <ipekgulsen.celik@gmail.com>
2023-04-19 16:13:48 +0300

    add generic repository

---
+++ Edukator.DataAccessLayer/Repository/GenericRepository.cs
@@ -0,0 +1,47 @@
+﻿using Edukator.DataAccessLayer.Abstract;
+using Edukator.DataAccessLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Edukator.DataAccessLayer.Repository
+{
+    public class GenericRepository<T> : IGenericDAL<T> where T : class
+    {
+
+        public void Delete(T entity)
+        {
+            using var context = new Context();
+            context.Remove(entity);
+            context.SaveChanges();
+        }
+
+        public T GetByID(int id)
+        {
+            using var context = new Context();
+            return context.Set<T>().Find(id);
+        }
+
+        public List<T> GetList()
+        {
+            using var context = new Context();
+            return context.Set<T>().ToList();
+        }
+
+        public void Insert(T entity)
+        {
+            using var context = new Context();
+            context.Add(entity);
+            context.SaveChanges();
+        }
+
+        public void Update(T entity)
+        {
+            using var context = new Context();
+            context.Update(entity);
+            context.SaveChanges();
+        }
+    }
+}
