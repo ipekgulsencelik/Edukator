833f96575f47e22953c027fe23bc67a9efc73f55
ipekgulsencelik <ipekgulsen.celik@gmail.com>
2023-04-24 20:33:00 +0300

    create controller

---
+++ Edukator.PresentationLayer/Controllers/AdminLayoutController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Edukator.PresentationLayer.Controllers
+{
+    public class AdminLayoutController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public PartialViewResult PartialHead()
+        {
+            return PartialView();
+        }
+    }
+}
---
+++ Edukator.PresentationLayer/Controllers/SocialMediaController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Edukator.PresentationLayer.Controllers
+{
+    public class SocialMediaController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
