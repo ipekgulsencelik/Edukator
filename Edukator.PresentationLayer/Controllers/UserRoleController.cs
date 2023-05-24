924fece9d475a7ac9ec61b2cde5d38542a88c8f3
ipekgulsencelik <ipekgulsen.celik@gmail.com>
2023-05-24 15:55:34 +0300

    20. lesson

---
+++ Edukator.PresentationLayer/Controllers/UserRoleController.cs
@@ -0,0 +1,35 @@
+﻿using Edukator.EntityLayer.Concrete;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Edukator.PresentationLayer.Controllers
+{
+    public class UserRoleController : Controller
+    {
+        private readonly RoleManager<AppRole> _roleManager;
+        private readonly UserManager<AppUser> _userManager;
+
+        public UserRoleController(RoleManager<AppRole> roleManager, UserManager<AppUser> userManager)
+        {
+            _roleManager = roleManager;
+            _userManager = userManager;
+        }
+
+        public IActionResult Index()
+        {
+            var values = _userManager.Users.ToList();
+
+            return View(values);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> AssignUserRole(int id)
+        {
+            var user = _userManager.Users.FirstOrDefault(x => x.Id == id);
+            var roles = _roleManager.Roles.ToList();
+            var userRoles = await _userManager.GetRolesAsync(user);
+
+            return View();
+        }
+    }
+}
